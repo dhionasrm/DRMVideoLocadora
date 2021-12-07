fe7eeebbc65794efdc38e78844256c57b9c4aa15
dhionas magalhães <dhionas17@hotmail.com>
2021-12-07 17:50:10 -0300

    Inicio do meu Primeiro Projeto

---
+++ DRMVideoLocadora/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace DRMVideoLocadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItem47 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueRayDVDsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.transaçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.locaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.devoluçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.janelaEmCascataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem1,
+            this.MenuItem3,
+            this.MenuItem8});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(714, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuItem1
+            // 
+            this.MenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem47,
+            this.toolStripSeparator1,
+            this.MenuItem2});
+            this.MenuItem1.Name = "MenuItem1";
+            this.MenuItem1.Size = new System.Drawing.Size(124, 20);
+            this.MenuItem1.Text = "DRMVideoLocadora";
+            this.MenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // MenuItem47
+            // 
+            this.MenuItem47.Name = "MenuItem47";
+            this.MenuItem47.Size = new System.Drawing.Size(180, 22);
+            this.MenuItem47.Text = "Fechar";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // MenuItem2
+            // 
+            this.MenuItem2.Name = "MenuItem2";
+            this.MenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.MenuItem2.Text = "Sair";
+            // 
+            // MenuItem3
+            // 
+            this.MenuItem3.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem4,
+            this.MenuItem5,
+            this.toolStripSeparator2,
+            this.MenuItem7});
+            this.MenuItem3.Name = "MenuItem3";
+            this.MenuItem3.Size = new System.Drawing.Size(49, 20);
+            this.MenuItem3.Text = "Editar";
+            // 
+            // MenuItem4
+            // 
+            this.MenuItem4.Name = "MenuItem4";
+            this.MenuItem4.Size = new System.Drawing.Size(180, 22);
+            this.MenuItem4.Text = "Incluir";
+            // 
+            // MenuItem5
+            // 
+            this.MenuItem5.Name = "MenuItem5";
+            this.MenuItem5.Size = new System.Drawing.Size(180, 22);
+            this.MenuItem5.Text = "Excluir";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // MenuItem7
+            // 
+            this.MenuItem7.Name = "MenuItem7";
+            this.MenuItem7.Size = new System.Drawing.Size(180, 22);
+            this.MenuItem7.Text = "Editar";
+            // 
+            // MenuItem8
+            // 
+            this.MenuItem8.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientesToolStripMenuItem,
+            this.blueRayDVDsToolStripMenuItem,
+            this.transaçõesToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.janelaEmCascataToolStripMenuItem});
+            this.MenuItem8.Name = "MenuItem8";
+            this.MenuItem8.Size = new System.Drawing.Size(68, 20);
+            this.MenuItem8.Text = "Visualizar";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // blueRayDVDsToolStripMenuItem
+            // 
+            this.blueRayDVDsToolStripMenuItem.Name = "blueRayDVDsToolStripMenuItem";
+            this.blueRayDVDsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.blueRayDVDsToolStripMenuItem.Text = "Blue-Ray | DVDs";
+            // 
+            // transaçõesToolStripMenuItem
+            // 
+            this.transaçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.locaçãoToolStripMenuItem,
+            this.devoluçãoToolStripMenuItem});
+            this.transaçõesToolStripMenuItem.Name = "transaçõesToolStripMenuItem";
+            this.transaçõesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.transaçõesToolStripMenuItem.Text = "Transações";
+            // 
+            // locaçãoToolStripMenuItem
+            // 
+            this.locaçãoToolStripMenuItem.Name = "locaçãoToolStripMenuItem";
+            this.locaçãoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.locaçãoToolStripMenuItem.Text = "Locação";
+            // 
+            // devoluçãoToolStripMenuItem
+            // 
+            this.devoluçãoToolStripMenuItem.Name = "devoluçãoToolStripMenuItem";
+            this.devoluçãoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.devoluçãoToolStripMenuItem.Text = "Devolução";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // janelaEmCascataToolStripMenuItem
+            // 
+            this.janelaEmCascataToolStripMenuItem.Name = "janelaEmCascataToolStripMenuItem";
+            this.janelaEmCascataToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.janelaEmCascataToolStripMenuItem.Text = "Janela em Cascata";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 520);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Relação de Clientes";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem47;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem5;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueRayDVDsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transaçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem locaçãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem devoluçãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem janelaEmCascataToolStripMenuItem;
+    }
+}
+
